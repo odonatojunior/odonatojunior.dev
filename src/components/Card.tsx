f9f8a9fc0ff8fc0a7cdfba262a25763dfb42506d
Donato Jr <donato.junior.07@gmail.com>
2023-01-26 21:38:22 -0300

    Revert "Revert "feat(Card): create card component""
    This reverts commit 8b71c32c1a0f462f0712feeb8211a9e00a87eea0.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,16 +4,22 @@ import { HiExternalLink } from 'react-icons/hi'
 
 type CardProps = {
   title: string
+  slug: string
+  tags?: string[]
 }
 
-export default function Card({ title }: CardProps) {
+export default function Card({ title, tags, slug }: CardProps) {
   return (
     <Link
-      href='#'
-      className='relative flex items-end justify-between gap-2 rounded-md bg-white/10 p-4 backdrop-blur-md transition hover:scale-105 hover:bg-white/25 hover:shadow-md hover:shadow-white/5 active:bg-white/20'
+      href={slug || '#'}
+      className='z-20 flex items-end justify-between gap-2 rounded-md bg-white/10 p-4 backdrop-blur-md transition hover:scale-105 hover:bg-white/25 active:bg-white/20'
     >
       <div className='flex flex-col items-start gap-2 '>
-        <Tag name='Next JS' />
+        <div className='flex flex-wrap gap-1'>
+          {tags?.map((tag) => (
+            <Tag key={tag} name={tag} />
+          ))}
+        </div>
         <h1 className='text-lg font-semibold leading-snug text-white'>
           {title}
         </h1>
