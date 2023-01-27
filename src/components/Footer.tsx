32569a21451bf765a866f27245364e31efbad0ce
Donato Jr <donato.junior.07@gmail.com>
2023-01-26 21:39:10 -0300

    feat(Footer): update footer info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,32 @@
 import Link from 'next/link'
 import { GrInstagram, GrTwitter, GrGithub } from 'react-icons/gr'
 
+export const SOCIAL_MEDIA_USER = 'odonatojunior'
+
 export default function Footer() {
   const date = new Date().getFullYear()
   return (
-    <footer className='relative z-20 mt-24 flex flex-col items-center justify-center gap-2'>
+    <footer className='container relative z-30 mx-auto mt-12 mb-8 flex flex-col items-center justify-center gap-2'>
       <div className='flex gap-2'>
-        <Link href='#'>
+        <Link
+          href={`https://instagram.com/${SOCIAL_MEDIA_USER}`}
+          target='_blank'
+          className='rounded-full p-3 hover:bg-white/10'
+        >
           <GrInstagram className='h-5 w-5 fill-white' />
         </Link>
-        <Link href='#'>
+        <Link
+          href={`https://twitter.com/${SOCIAL_MEDIA_USER}`}
+          target='_blank'
+          className='rounded-full p-3 hover:bg-white/10'
+        >
           <GrTwitter className='h-5 w-5 fill-white' />
         </Link>
-        <Link href='#'>
+        <Link
+          href={`https://github.com/${SOCIAL_MEDIA_USER}`}
+          target='_blank'
+          className='rounded-full p-3 hover:bg-white/10'
+        >
           <GrGithub className='h-5 w-5 fill-white' />
         </Link>
       </div>
