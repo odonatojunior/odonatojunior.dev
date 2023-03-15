6946fe7429c48626798ce6a80e1c8681b5f3e55d
Donato Jr <donato.junior.07@gmail.com>
2023-03-15 11:11:29 -0300

    feat(markdown): add highlight plugin to showdown

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,6 +1,16 @@
 import Showdown from 'showdown'
+import ShowdownHighLight from 'showdown-highlight'
 
 export default function markdownCompiler(markdown: string) {
-  const converter = new Showdown.Converter()
+  const converter = new Showdown.Converter({
+    ghCodeBlocks: true,
+    openLinksInNewWindow: true,
+    extensions: [
+      ShowdownHighLight({
+        pre: true,
+        auto_detection: true
+      })
+    ]
+  })
   return converter.makeHtml(markdown)
 }
